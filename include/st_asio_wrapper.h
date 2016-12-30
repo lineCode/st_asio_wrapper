4df54f1352ea10c5e3ff429bd5a115d46c2bcb0a
youngwolf <mail2tao@163.com>
2016-12-30 13:09:01 +0800

    Fix release notes.

--- include/st_asio_wrapper.h
+++ include/st_asio_wrapper.h
@@ -124,7 +124,7 @@
  * Support scatter-gather buffers when receiving messages, this feature needs modification of i_unpacker, you must explicitly define
  *  ST_ASIO_SCATTERED_RECV_BUFFER macro to open it, this is just for compatiblity.
  * Simplify lock-free mechanism.
- * Optimize container insertion (use series of emplace functions instead) in normal edition.
+ * Optimize container insertion (use series of emplace functions instead).
  * Demo test_client support alterable number of sending thread (before, it's a hard code 16).
  * Fix bug: In extreme cases, messages may get starved in receive buffer and will not be dispatched until arrival of next message.
  * Fix bug: In extreme cases, messages may get starved in send buffer and will not be sent until arrival of next message.
